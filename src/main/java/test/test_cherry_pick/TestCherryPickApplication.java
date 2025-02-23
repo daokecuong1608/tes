0e34c7f9970c649b5522ec7c14d2652b4c1158e1
Đào Kế Cương-Intern <daokecuong1608@gmail.com>
2025-02-23 21:47:39 +0700

    first commit

---
+++ src/main/java/test/test_cherry_pick/TestCherryPickApplication.java
@@ -0,0 +1,13 @@
+package test.test_cherry_pick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCherryPickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCherryPickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/test/test_cherry_pick/TestCherryPickApplicationTests.java
@@ -0,0 +1,13 @@
+package test.test_cherry_pick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCherryPickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
